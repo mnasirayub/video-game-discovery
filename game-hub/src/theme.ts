b0e5169b6a08a0245ec79540dc36c040d3fe6d5f
mnasirayub <nasir138g@gmail.com>
2024-08-18 13:27:59 +0500

    dark mode theme implementation

---
+++ game-hub/src/theme.ts
@@ -0,0 +1,11 @@
+// theme.ts
+
+import { extendTheme, type ThemeConfig } from '@chakra-ui/react'
+
+const config: ThemeConfig = {
+    initialColorMode: 'dark',
+}
+
+const theme = extendTheme({ config })
+
+export default theme
